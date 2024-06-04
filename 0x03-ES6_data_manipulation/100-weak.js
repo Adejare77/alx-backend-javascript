c2d03c767abde79e0a7773c42032a469240b17e9
Adejare <adejare.arasheed@gmail.com>
2024-06-04 13:25:16 +0100

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,12 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  if (endpoint instanceof Object) {
-    if (weakMap.has(endpoint)) {
-      const count = weakMap.get(endpoint);
-      if (count >= 5) {
-        throw new Error('Endpoint load is high');
-      }
-      weakMap.set(endpoint, count + 1);
-    } else {
-      weakMap.set(endpoint, 1);
+  if (weakMap.has(endpoint)) {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
     }
+    weakMap.set(endpoint, count + 1);
+  } else {
+    weakMap.set(endpoint, 1);
   }
 }
