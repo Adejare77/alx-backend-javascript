d0e19b08a631e442998346b1390a4d4f35348746
Adejare <adejare.arasheed@gmail.com>
2024-06-04 12:50:19 +0100

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,9 @@
 export default function updateStudentGradeByCity(array, city, newGrades) {
-  const updatedArray = array.map((obj) => {
-    const score = newGrades.filter((student) => student.studentId === obj.id)[0];
-    return { ...obj, grade: score ? score.grade : 'N/A' };
-  });
+  const updatedArray = array
+    .filter((CityStudents) => CityStudents.location === city)
+    .map((obj) => {
+      const score = newGrades.filter((student) => student.studentId === obj.id)[0];
+      return { ...obj, grade: score ? score.grade : 'N/A' };
+    });
   return updatedArray;
 }
