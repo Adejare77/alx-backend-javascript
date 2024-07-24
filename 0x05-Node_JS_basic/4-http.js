4adc50bc6dd71043e996ed779d681f6505526b03
Adejare <adejare.arasheed@gmail.com>
2024-07-25 00:17:44 +0100

    NodeJS Basics

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,16 @@
-const http = require('http');
+#!/usr/bin/node
+
+const { createServer } = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello Holberton School!');
+const app = createServer((request, response) => {
+  if (response.statusCode === 200) {
+    response.end('Hello Holberton School!');
+  }
 });
 
-app.listen(port, hostname, () => {
-});
+app.listen(port, hostname);
 
 module.exports = app;
