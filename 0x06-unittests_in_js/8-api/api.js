39e35b702101ce6ca12a3848308419ed5340579d
Adejare <adejare.arasheed@gmail.com>
2024-07-28 19:41:53 +0100

    Unittest in JS

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
-const app = express();
+app = express();
 const port = 7865;
+const hostname = 'localhost';
 
-app.get('/', (request, response) => {
-  response.send('Welcome to the payment system');
-});
+app.get('/', (req, resp) => {
+  resp.status(200).send('Welcome to the payment system');
+})
 
-app.listen(port, () => {
-    console.log("API available on localhost port 7865");
-});
+app.listen(port, hostname, () => {
+  console.log('API available on localhost port 7865')
+})
 
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,54 @@
-const request = require("request");
-const {describe, it} = require("mocha");
-const expect = require("chai").expect;
+const { describe, it } = require('mocha');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = require('assert');
+const app = require('./api')
 
-describe("Index page", function() {
-    const options = {
-	url: "http://localhost:7865/",
-	method: "GET"
-    }
-    it("check correct status code", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.statusCode).to.equal(200);
-	    done();
-	});
+chai.use(chaiHttp);
+
+describe('Index page', () => {
+  it('Check the response status code', (done) => {
+    chai.request(app)
+    .get('/')
+    .end((err, resp) => {
+      chai.expect(resp.statusCode).to.equal(200);
+      done();
     });
-    it("check correct content", function(done) {
-	request(options, function(err, res, body) {
-	    expect(body).to.contain("Welcome to the payment system");
-	    done();
-	});
+  });
+
+  it('Check the response content', (done) => {
+    chai.request(app)
+    .get('/')
+    .end((err, resp) => {
+      chai.expect(resp.text).to.equal('Welcome to the payment system');
+      done();
     });
-    it("check correct content length", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.headers['content-length']).to.equal('29');
-	    done();
-	});
+  });
+
+  it('Check the Content Type', (done) => {
+    chai.request(app)
+    .get('/')
+    .end((err, resp) => {
+      assert.equal(resp.header['content-type'], 'text/html; charset=utf-8');
+      done();
     });
-});
+  });
+
+  it('Check the Content Length', (done) => {
+    chai.request(app)
+    .get('/')
+    .end((err, resp) => {
+      assert.equal(resp.header['content-length'], '29');
+      done();
+    });
+  });
+
+  it('Check the web Server', (done) => {
+    chai.request(app)
+    .get('/')
+    .end((err, resp) => {
+      assert.equal(resp.header['x-powered-by'], 'Express');
+      done();
+    });
+  });
+})
