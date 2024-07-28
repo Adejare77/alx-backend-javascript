5301f70647e6439774434a04489211cb243bbf14
Adejare <adejare.arasheed@gmail.com>
2024-07-28 17:18:32 +0100

    Unittest in JS

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,23 +1,44 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./5-payment");
-const expect = require("chai").expect;
+const sinon = require('sinon');
+const { describe, it, beforeEach, afterEach } = require('mocha')
+const assert = require('assert')
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils')
 
-describe("sendPaymentRequestToApi", function() {
-    beforeEach("Set up spy to use for each test", function() {
-	sinon.spy(console, "log");
-    });
-    afterEach("restore spy after each test", function() {
-	console.log.restore();
-    });
-    it("check that console.log is called with the right arg", function() {
-	sendPaymentRequestToApi(100, 20);
 
-	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
-    });
-    it("check that console.log is called with the right arg", function() {
-	sendPaymentRequestToApi(10, 10);
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberSpy;
+  let consoleSpy;
+  beforeEach(() => {
+    // Spy on calculateNumber method of Utils
+    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber')
+    // Spy on console.log
+    consoleSpy = sinon.spy(console, 'log')
+  });
 
-	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
-    });
+  afterEach(() => {
+    // Restore all the original function
+    calculateNumberSpy.restore();
+    consoleSpy.restore();
+  });
+
+  it('Checks that sendPaymentRequestToApi is called', () => {
+    // Call sendPaymentRequestToApi to check if it inherits/calls the Utils.calculateNumber
+    sendPaymentRequestToApi(100, 20)
+    // check if Utils.calculateNumber was called once with correct args
+    assert(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20));
+    // Verify the console is called onec
+    assert(consoleSpy.calledOnceWithExactly('The total is: 120'));
+  });
+
+    it('Checks that sendPaymentRequestToApi is called', () => {
+      // Call sendPaymentRequestToApi to check if it inherits/calls the Utils.calculateNumber
+      sendPaymentRequestToApi(10, 10)
+      // check if Utils.calculateNumber was called once with correct args
+      assert(calculateNumberSpy.calledOnceWithExactly('SUM', 10, 10));
+      // Verify the console is called onec
+      assert(consoleSpy.calledOnceWithExactly('The total is: 20'));
+    })
 });
+
+
+
