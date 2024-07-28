d7795d27c1d21d7e936361fbc9f01bbdb350ff3d
Adejare <adejare.arasheed@gmail.com>
2024-07-28 19:58:45 +0100

    Unittest in JS

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,13 +2,12 @@ const express = require('express');
 
 app = express();
 const port = 7865;
-const hostname = 'localhost';
 
 app.get('/', (req, resp) => {
   resp.status(200).send('Welcome to the payment system');
 })
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log('API available on localhost port 7865')
 })
 
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,53 +1,94 @@
 const { describe, it } = require('mocha');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
+const request = require('request');
 const assert = require('assert');
-const app = require('./api')
+const app = require('./api');
+const { response } = require('express');
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
+
+// describe('Index page', () => {
+//   it('Check the response status code', (done) => {
+//     chai.request(app)
+//     .get('/')
+//     .end((err, resp) => {
+//       chai.expect(resp.statusCode).to.equal(200);
+//       done();
+//     });
+//   });
+
+//   it('Check the response content', (done) => {
+//     chai.request(app)
+//     .get('/')
+//     .end((err, resp) => {
+//       chai.expect(resp.text).to.equal('Welcome to the payment system');
+//       done();
+//     });
+//   });
+
+//   it('Check the Content Type', (done) => {
+//     chai.request(app)
+//     .get('/')
+//     .end((err, resp) => {
+//       assert.equal(resp.header['content-type'], 'text/html; charset=utf-8');
+//       done();
+//     });
+//   });
+
+//   it('Check the Content Length', (done) => {
+//     chai.request(app)
+//     .get('/')
+//     .end((err, resp) => {
+//       assert.equal(resp.header['content-length'], '29');
+//       done();
+//     });
+//   });
+
+//   it('Check the web Server', (done) => {
+//     chai.request(app)
+//     .get('/')
+//     .end((err, resp) => {
+//       assert.equal(resp.header['x-powered-by'], 'Express');
+//       done();
+//     });
+//   });
+// })
+
 
 describe('Index page', () => {
+  const url = 'http://localhost:7865';
+
   it('Check the response status code', (done) => {
-    chai.request(app)
-    .get('/')
-    .end((err, resp) => {
-      chai.expect(resp.statusCode).to.equal(200);
+    request(url, (err, response) => {
+      assert.equal(response.statusCode, 200);
       done();
     });
   });
 
   it('Check the response content', (done) => {
-    chai.request(app)
-    .get('/')
-    .end((err, resp) => {
-      chai.expect(resp.text).to.equal('Welcome to the payment system');
+    request(url, (err, response) => {
+      chai.expect(response.body).to.contain('Welcome to the payment system');
       done();
-    });
+    })
   });
 
   it('Check the Content Type', (done) => {
-    chai.request(app)
-    .get('/')
-    .end((err, resp) => {
-      assert.equal(resp.header['content-type'], 'text/html; charset=utf-8');
+    request(url, (err, resp) => {
+      assert.equal(resp.headers['content-type'], 'text/html; charset=utf-8');
       done();
     });
   });
 
   it('Check the Content Length', (done) => {
-    chai.request(app)
-    .get('/')
-    .end((err, resp) => {
-      assert.equal(resp.header['content-length'], '29');
+    request(url, (err, resp) => {
+      assert.equal(resp.headers['content-length'], '29');
       done();
     });
   });
 
   it('Check the web Server', (done) => {
-    chai.request(app)
-    .get('/')
-    .end((err, resp) => {
-      assert.equal(resp.header['x-powered-by'], 'Express');
+    request(url, (err, resp) => {
+      assert.equal(resp.headers['x-powered-by'], 'Express');
       done();
     });
   });
