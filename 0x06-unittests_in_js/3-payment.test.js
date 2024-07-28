936e33144422f51d65e85e3e97fa35e2e2655053
Adejare <adejare.arasheed@gmail.com>
2024-07-28 16:35:25 +0100

    Unittest in JS

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,25 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./3-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const sinon = require('sinon');
+const { describe, it } = require('mocha')
+const assert = require('assert')
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils')
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber was called once", function() {
-	const spy = sinon.spy(Utils, "calculateNumber");
 
-	sendPaymentRequestToApi(50, 24.52);
+describe('sendPaymentRequestToApi', () => {
+  it('Checks that sendPaymentRequestToApi is called', () => {
+    // create a spy for Utils.calculateNumber
+    const spy = sinon.spy(Utils, 'calculateNumber')
 
-	assert(spy.calledOnce);
-	spy.restore();
-    });
+    // Call sendPaymentRequestToApi to check if it inherits/calls the Utils.calculateNumber
+    sendPaymentRequestToApi(100, 20);
+
+    // check if Utils.calculateNumber was called once with correct args
+    assert(spy.calledOnceWithExactly('SUM', 100, 20));
+
+    // Restore the original function
+    spy.restore();
+  });
 });
+
+
+
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,33 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./4-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const sinon = require('sinon');
+const { describe, it } = require('mocha')
+const assert = require('assert')
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils')
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
 
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
-    });
+describe('sendPaymentRequestToApi', () => {
+  it('Checks that sendPaymentRequestToApi is called', () => {
+    // stub the calculateNumber method of Utils to always return 10
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber')
+    calculateNumberStub.returns(10);
+
+    // Spy on Console.log
+    const consoleSpy = sinon.spy(console, 'log')
+
+    // Call sendPaymentRequestToApi to check if it inherits/calls the Utils.calculateNumber
+    sendPaymentRequestToApi(100, 20)
+
+    // check if Utils.calculateNumber was called once with correct args
+    assert(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20));
+
+    // Verify the return value of sendPaymentRequestToApi from console
+    assert(consoleSpy.calledOnceWithExactly('The total is: 10'));
+
+    // Restore all the original function
+    calculateNumberStub.restore()
+    consoleSpy.restore();
+  });
 });
+
+
+
