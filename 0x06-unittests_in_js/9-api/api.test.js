1e0bc20ddaf014973c83effc1fdd820600f1c7cd
Adejare <adejare.arasheed@gmail.com>
2024-08-27 10:27:36 +0100

    Test APIs

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,32 +1,43 @@
-const { describe, it } = require('mocha');
-const chai = require('chai');
-const request = require('request');
-const app = require('./api');
-const { response } = require('express');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('Index page', () => {
-  const url = 'http://localhost:7865';
-
-  it('Check the response status code', (done) => {
-    request(url, (err, response) => {
-      chai.expect(response.statusCode).to.equal(200);
-      done();
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.equal("Welcome to the payment system");
+	    done();
+	});
+    });
+});
 
-  it('Check the response content', (done) => {
-    request(url, (err, response) => {
-      chai.expect(response.body).to.contain('Welcome to the payment system');
-      done();
-    })
-  });
-
-
-  it('should include the correct parameter', (done) => {
-    const param = 124
-    request(`http://localhost:7865/cart/${124}`, (err, resp, body) => {
-      chai.expect(body).to.contain(`Payment methods for cart 124`)
-      done()
-    })
-  })
-})
+describe("Cart page", function() {
+    it("check correct status code for correct url", function(done) {
+	request.get("http://localhost:7865/cart/12", function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content for correct url", function(done) {
+	request.get("http://localhost:7865/cart/12", function(err, res, body) {
+	    expect(body).to.contain("Payment methods for cart 12");
+	    done();
+	});
+    });
+    it("check correct status code for incorrect url", function(done) {
+	request.get("http://localhost:7865/cart/kim", function(err, res, body) {
+	    expect(res.statusCode).to.equal(404);
+	    done();
+	});
+    });
+});
