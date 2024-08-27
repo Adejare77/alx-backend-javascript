91ac0c4eee070620d45cdd02a2992f26f36aba58
Adejare <adejare.arasheed@gmail.com>
2024-08-27 09:04:25 +0100

    Api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,66 +1,15 @@
 const { describe, it } = require('mocha');
 const chai = require('chai');
 const request = require('request');
-const assert = require('assert');
 const app = require('./api');
 const { response } = require('express');
-
-// chai.use(chaiHttp);
-
-// describe('Index page', () => {
-//   it('Check the response status code', (done) => {
-//     chai.request(app)
-//     .get('/')
-//     .end((err, resp) => {
-//       chai.expect(resp.statusCode).to.equal(200);
-//       done();
-//     });
-//   });
-
-//   it('Check the response content', (done) => {
-//     chai.request(app)
-//     .get('/')
-//     .end((err, resp) => {
-//       chai.expect(resp.text).to.equal('Welcome to the payment system');
-//       done();
-//     });
-//   });
-
-//   it('Check the Content Type', (done) => {
-//     chai.request(app)
-//     .get('/')
-//     .end((err, resp) => {
-//       assert.equal(resp.header['content-type'], 'text/html; charset=utf-8');
-//       done();
-//     });
-//   });
-
-//   it('Check the Content Length', (done) => {
-//     chai.request(app)
-//     .get('/')
-//     .end((err, resp) => {
-//       assert.equal(resp.header['content-length'], '29');
-//       done();
-//     });
-//   });
-
-//   it('Check the web Server', (done) => {
-//     chai.request(app)
-//     .get('/')
-//     .end((err, resp) => {
-//       assert.equal(resp.header['x-powered-by'], 'Express');
-//       done();
-//     });
-//   });
-// })
-
 
 describe('Index page', () => {
   const url = 'http://localhost:7865';
 
   it('Check the response status code', (done) => {
     request(url, (err, response) => {
-      assert.equal(response.statusCode, 200);
+      chai.expect(response.statusCode, 200);
       done();
     });
   });
@@ -74,21 +23,21 @@ describe('Index page', () => {
 
   it('Check the Content Type', (done) => {
     request(url, (err, resp) => {
-      assert.equal(resp.headers['content-type'], 'text/html; charset=utf-8');
+      chai.expect(resp.headers['content-type'], 'text/html; charset=utf-8');
       done();
     });
   });
 
   it('Check the Content Length', (done) => {
     request(url, (err, resp) => {
-      assert.equal(resp.headers['content-length'], '29');
+      chai.expect(resp.headers['content-length'], '29');
       done();
     });
   });
 
   it('Check the web Server', (done) => {
     request(url, (err, resp) => {
-      assert.equal(resp.headers['x-powered-by'], 'Express');
+      chai.expect(resp.headers['x-powered-by'], 'Express');
       done();
     });
   });
